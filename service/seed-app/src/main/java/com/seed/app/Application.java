62e9716628134b8315cd5fcc4b16caf6ed5fe92e
mhmtyilmaz <mylmz89@gmail.com>
2017-09-03 15:08:36 +0300

    implement configurations and project ready to run

--- service/seed-app/src/main/java/com/seed/app/Application.java
+++ service/seed-app/src/main/java/com/seed/app/Application.java
@@ -1,15 +1,15 @@
-package com.ingbank.assesment;
-dapter;
+package com.seed.app;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     @Autowired
-    private Environment env;
     public static void main(String[] args) {
-
         SpringApplication.run(Application.class, args);
-
     }
 
 }
