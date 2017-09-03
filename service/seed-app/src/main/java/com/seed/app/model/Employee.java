f770cf800e186dac8d117aa46ffe5e8c74695f28
mhmtyilmaz <mylmz89@gmail.com>
2017-09-03 15:46:52 +0300

    implement Employee model class and import lombok

---
+++ service/seed-app/src/main/java/com/seed/app/model/Employee.java
@@ -0,0 +1,41 @@
+package com.seed.app.model;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * Created by MEHMET on 3.9.2017.
+ */
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "BIRTH_DATE")
+    private Date birthDate;
+
+    @Column(name = "EDUCATION")
+    private Integer education;
+
+    @Column(name = "DEPT_NAME")
+    private Integer deptName;
+
+    @Column(name = "TITLE")
+    private Integer title;
+}
