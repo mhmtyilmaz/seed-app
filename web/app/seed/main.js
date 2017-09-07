00eba0782a524faea52acd396decb9e1f917fe07
mhmtyilmaz <mylmz89@gmail.com>
2017-09-07 20:01:23 +0300

    Improve Ui birth date field and add required check for all input

--- web/app/seed/main.js
+++ web/app/seed/main.js
@@ -37,10 +37,10 @@ angular.module('myApp.main', ['ngRoute'])
             return defered.promise;
         };
 
-        var addEmployee = function (data) {
+        var addOrUpdateEmployee = function (data) {
             var defered = $q.defer();
             $http({
-                method: 'PUT',
+                method: data.id == undefined ? 'PUT' :'POST',
                 url: baseUrl + '/api/employee',
                 data: data
             }).then(function (response) {
@@ -51,31 +51,14 @@ angular.module('myApp.main', ['ngRoute'])
             });
             return defered.promise;
         };
-
-        var updateEmployee = function (data) {
-            var defered = $q.defer();
-            $http({
-                method: 'POST',
-                url: baseUrl + '/api/employee',
-                data: data
-            }).then(function (response) {
-                defered.resolve(response.data);
-            }, function (error) {
-                $log.error(error.message);
-                defered.reject(error);
-            });
-            return defered.promise;
-        };
-
 
         getAllEmployee().then(function (resp) {
              for (var i = 0; i < resp.data.length; i++) {
-             resp.data[i].birthDate = dateFormat(resp.data[i].birthDate);
+                    resp.data[i].birthDate = new Date(resp.data[i].birthDate);
              }
             $scope.gridOptions.data = resp.data;
             $scope.isNoData = resp.data.length;
         });
-
 
         $scope.gridOptions = {
             columnDefs: [
@@ -85,49 +68,50 @@ angular.module('myApp.main', ['ngRoute'])
                 {field: 'deptName', name: 'Department Name'},
                 {field: 'title', name: 'Title'}
             ],
-            enableFullRowSelection: true,
-            enableRowSelection: true,
-            enableSelectAll: false,
+            enableRowSelection: false,
+            enableRowHeaderSelection: true,
             multiSelect: false
         };
 
         $scope.gridOptions.onRegisterApi = function(gridApi) {
             $scope.gridApi = gridApi;
             gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+                $scope.id = row.entity.id;
                 $scope.name = row.entity.name;
                 $scope.birthDate = row.entity.birthDate;
                 $scope.education = row.entity.education;
                 $scope.deptName = row.entity.deptName;
                 $scope.title = row.entity.title;
+                $scope.buttonName = $scope.id != undefined ? "Update Employee" : "Add Employee";
 
             });
         }
 
-        var dateFormat = function (timeStamp) {
-            var d = new Date(timeStamp);
-            var dformat = [
-                d.getDate(),
-                d.getMonth() + 1,
-                d.getFullYear()].join('/');
 
-            return dformat;
-        };
         $scope.saveEmployee = function () {
-
             var newData = {
+                id : $scope.id,
                 name: $scope.name,
                 birthDate: $scope.birthDate,
                 education: $scope.education,
                 deptName : $scope.deptName,
                 title : $scope.title
             };
-            addEmployee(newData).then(function (response) {
+
+            addOrUpdateEmployee(newData).then(function (response) {
                 getAllEmployee().then(function (resp) {
                     for (var i = 0; i < resp.data.length; i++) {
-                        resp.data[i].birthDate = dateFormat(resp.data[i].birthDate);
+                        resp.data[i].birthDate = new Date(resp.data[i].birthDate);
                     }
                     $scope.gridOptions.data = resp.data;
                     $scope.isNoData = response.data.length;
+                    $scope.id = null;
+                    $scope.name = null;
+                    $scope.birthDate = null;
+                    $scope.education = null;
+                    $scope.deptName = null;
+                    $scope.title = null;
+                    $scope.buttonName = "Add Employee";
                 });
             });
         }
